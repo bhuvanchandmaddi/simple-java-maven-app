cd0fa43757ea652d2d98840e0c384c62bded6058
bhuvanchand <39622224+bhuvanchandmaddi@users.noreply.github.com>
2023-02-21 20:19:25 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!..This is new build";
+    private static final String MESSAGE = "Hello World!..This is new build..using webhook";
 
     public App() {}
 
